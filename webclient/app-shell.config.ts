31631ba5aafc99c4e1c8486158a6d39a8832961a
David Santos <david.santos@hitachivantara.com>
2024-05-28 14:40:15 +0100

    amazing home start

--- webclient/app-shell.config.ts
+++ webclient/app-shell.config.ts
@@ -7,23 +7,29 @@ export default (): HvAppShellConfig => ({
 
   header: {
     actions: [
-      { bundle: "@hv/help-client/button.js",
+      {
+        bundle: "@hv/help-client/button.js",
         config: { url: "https://www.hitachivantara.com/", description: "Hitachi Vantara Help Link" }
       },
-      { bundle: "@hv/theming-client/colorModeSwitcher.js"      },
-      { bundle: "@hv/app-switcher-client/toggle.js",
-        config: { title: "Apps",
-									apps: [
-										{ label: "App 1", description: "Application 1", url: "#", target: "NEW" },
-										{ label: "App 2", description: "Application 2", url: "#", target: "SELF", icon: { iconType: "uikit", name: "Warehouse" } }
-									]}
+      { bundle: "@hv/theming-client/colorModeSwitcher.js" },
+      {
+        bundle: "@hv/app-switcher-client/toggle.js",
+        config: {
+          title: "Apps",
+          apps: [
+            { label: "App 1", description: "Application 1", url: "#", target: "NEW" },
+            { label: "App 2", description: "Application 2", url: "#", target: "SELF", icon: { iconType: "uikit", name: "Warehouse" } }
+          ]
+        }
       }
     ]
   },
 
   mainPanel: {
     maxWidth: "xl",
-    views: [{bundle: "@self/pages/Welcome.js", route: "/index.html"}]
+    views: [{
+      bundle: "@self/pages/Welcome.js", route: "/index.html"
+    }]
   },
 
 });
